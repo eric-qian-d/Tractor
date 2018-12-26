5fb9b9c2e83bf72d002311326cb12bbb9221d04e
eric-qian-d <eric.qian.d@gmail.com>
2018-12-25 21:58:16 -0600

    render cards in sorted order, card selection has animation

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,64 @@
 var socket = io();
+
+var hand1; //spades
+var hand2; //hearts
+var hand3; //clubs
+var hand4; //diamonds
+var trump;
+
+var selected = new Map;
+
+var stringToCard = new Map;
 // socket.on('message', function(data) {
 //   console.log(data);
 // });
+function renderHandDealing() {
+  console.log('rendering hand');
+  trump.sort(function(a, b){return a.power - b.power});
+  hand1.sort(function(a, b){return a.power - b.power});
+  hand2.sort(function(a, b){return a.power - b.power});
+  hand3.sort(function(a, b){return a.power - b.power});
+  hand4.sort(function(a, b){return a.power - b.power});
+  var spadesDiv = document.getElementById('spadesDiv');
+  var heartsDiv = document.getElementById('heartsDiv');
+  var clubsDiv = document.getElementById('clubsDiv');
+  var diamondsDiv = document.getElementById('diamondsDiv');
+  var trumpDiv = document.getElementById('trumpDiv');
+  renderSuit(hand1, spadesDiv);
+  renderSuit(hand2, heartsDiv);
+  renderSuit(hand3, clubsDiv);
+  renderSuit(hand4, diamondsDiv);
+  renderSuit(trump, trumpDiv);
+
+  console.log(hand1, hand2, hand3, hand4, trump);
+};
+
+function renderSuit(cards, div) {
+	while (div.firstChild) {
+		div.removeChild(div.firstChild);
+	}
+	for(var i = 0; i < cards.length; i++) {
+		var newCard = document.createElement('img');
+		newCard.setAttribute('style', 'width:80px; height:80px');
+		//images from https://code.google.com/archive/p/vector-playing-cards/downloads
+		newCard.setAttribute('src', '/static/images/cards/' + cards[i].value.toString() + '-' + cards[i].suit.toString() + '.png');
+		newCard.setAttribute('id', cards[i].value.toString() + '-' + cards[i].suit.toString() + '-' + cards[i].deck.toString());
+		newCard.addEventListener('click', function() {
+			if (selected.get(cards[i])) {
+				selected.delete(cards[i]);
+				this.style.border = '0px';
+			}
+			else {
+				selected.set(cards[i], true) 
+				this.style.border = '1px';
+				this.style.borderColor = 'black';
+				this.style.borderStyle = 'solid';
+			}
+		});
+		stringToCard.set(cards[i].value.toString() + '-' + cards[i].suit.toString() + '-' + cards[i].deck.toString(), cards[i]);
+		div.append(newCard); 
+	}
+}
 
 document.getElementById('createButton').addEventListener('click', function(){
   var IdDiv = document.getElementById('inputBox');
@@ -14,7 +71,12 @@ document.getElementById('joinButton').addEventListener('click', function(){
   var IdDiv = document.getElementById('inputBox');
   var gameId = IdDiv.value;
   console.log('clicked join button');
-	socket.emit('join game', gameId);
+    socket.emit('join game', gameId);
+});
+
+document.getElementById('playButton').addEventListener('click', function() {
+  console.log('clicked play button');
+  socket.emit('play', 1);
 });
 
 socket.on('game created', function(data){
@@ -44,15 +106,89 @@ socket.on('game already joined', function(data){
 
 socket.on('game initializing', function(data){
   console.log('game init!');
+  hand1 = []; //spades
+  hand2 = []; //hearts
+  hand3 = []; //clubs
+  hand4 = []; //diamonds
+  trump = [];
   console.log(data);
 });
 
-socket.on('hand', function(hand) {
-  console.log('hand ');
+socket.on('deal card', function(data) {
+  // console.log('hand ', data[0]);
+  var card = data[0];
+  var suit = card.suit;
+  var value = card.value;
+  if(value == data[1] || suit == 'T') {
+    trump.push(card);
+    trump.sort();
+  }
+  else if(suit == 1){
+    hand1.push(card);
+  }
+  else if(suit == 2){
+    hand2.push(card);
+  }
+  else if(suit == 3){
+    hand3.push(card);
+  }
+  else{
+    hand4.push(card);
+  }
+  renderHandDealing();
 });
 
-socket.on('a', function(data) {
+socket.on('cards dealt', function(data) {
   console.log('all dealt!');
-  console.log(data);
+});
+
+socket.on('time', function(time) {
+  console.log('time', time);
+});
+
+socket.on('turn', function(turn) {
+  console.log('it is player ', turn, ' turn');
+});
+
+socket.on('finalize hand', function(data) {
+	console.log('finalizing hand!');
+  hand1 = []; //spades
+  hand2 = []; //hearts
+  hand3 = []; //clubs
+  hand4 = []; //diamonds
+  trump = [];
+  cards = data[0];
+  console.log(hand1, hand2, hand3, hand4, trump);
+  console.log(data[1], data[2]);
+  for(var i = 0; i < 25; i++) {
+    var card = cards[i];
+    var suit = card.suit;
+    var value = card.value;
+    if(suit == data[1] || value == data[2] || suit == 'T') {
+      if(value == data[2]) {
+        if(suit == data[1]) {
+          card.power = 16;
+        }
+        else {
+          card.power = 15;
+        }
+      }
+      trump.push(card);   
+    }
+    else if(suit == 1){
+      hand1.push(card);
+    }
+    else if(suit == 2){
+      hand2.push(card);
+    }
+    else if(suit == 3){
+      hand3.push(card);
+    }
+    else{
+      hand4.push(card);
+    }
+    
+  }
+  renderHandDealing();
 });
 
--- source/app.js
+++ source/app.js
@@ -9,15 +9,12 @@ var io = socketIO(server);
 
 var pages = require('./routes/pages.js');
 
-
 app.use('/', pages);
-
 
 app.set('port', 3000);
 app.use('/static', express.static('public'));
 
 // Routing
-
 
 // Starts the server.
 server.listen(3000, function() {
@@ -28,6 +25,7 @@ server.listen(3000, function() {
 
 var liveGames = new Map;
 var waitingGames = new Map;
+var players = new Map;
 io.on('connection', function(socket) {
   socket.on('create game', function(gameId) {
   	console.log('creating game...');
@@ -42,6 +40,7 @@ io.on('connection', function(socket) {
   			playerOrder : [socket.id]
   		}
   		waitingGames.set(gameId, game);
+  		players.set(socket.id, gameId);
   		io.to(socket.id).emit('game created', gameId);
   		console.log('successfully created');
   	}
@@ -57,6 +56,7 @@ io.on('connection', function(socket) {
     	} 
     	else{
 	    	socket.join(gameId);
+	    	players.set(socket.id, gameId);
 	    	game.players.set(socket.id, game.players.size + 1);
 	    	game.playerOrder.push(socket.id);
 	    	io.to(socket.id).emit('game joined', gameId);
@@ -65,31 +65,33 @@ io.on('connection', function(socket) {
 	    		io.to(gameId).emit('game initializing', gameId);
 	    		// io.to(gameId).emit('a', gameId);
 	    		console.log('initializing game for ', gameId);
-	    		waitingGames.delete(gameId);
-
-	    		var hand1;
-	    		var hand2;
-	    		var hand3;
-	    		var hand4;
-	    		var bottom;
-	    		[hand1, hand2, hand3, hand4, bottom] = initHands();
+	    		waitingGames.delete(gameId);	    		
 	    		var newLiveGame = {
 	    			id : gameId,
 	    			players : game.players, //Map of socket.id => number joined
-	    			order : [1, 2, 3, 4, 1, 2, 3, 4],
+	    			order : [null, 1, 2, 3, 4, 1, 2, 3, 4],
 	    			p1 : new Map([['id', game.playerOrder[0]],
-	    				['hand', hand1], ['level', 2]]),
+	    				['hand', null], ['level', 2]]),
 	    			p2 : new Map([['id', game.playerOrder[1]],
-	    				['hand', hand2], ['level', 2]]),
+	    				['hand', null], ['level', 2]]),
 	    			p3 : new Map([['id', game.playerOrder[2]],
-	    				['hand', hand3], ['level', 2]]),
+	    				['hand', null], ['level', 2]]),
 	    			p4 : new Map([['id', game.playerOrder[3]],
-	    				['hand', hand4], ['level', 2]]),
+	    				['hand', null], ['level', 2]]),
+	    			bottom : null,
 	    			trumpNum : null,
-	    			trumpSuit : null
+	    			trumpSuit : null,
+	    			host : null,
+	    			lastWinners : null,
+	    			cardsPlayed : 0,
+	    			roundStartingPlayer : 1,
+	    			roundPlayersPlayed : 0,
+	    			roundNumCards : null,
+	    			roundSuit : null
 	    		};
-	    		liveGames[gameId] = newLiveGame;
-	    		sendCards(newLiveGame);
+	    		liveGames.set(gameId,newLiveGame);
+
+	    		newGame(gameId);
 	    		}
 	    	}
     	}
@@ -97,66 +99,147 @@ io.on('connection', function(socket) {
     	io.to(socket.id).emit('invalid id', gameId);
     }
   });
+  socket.on('play', function(cards) {
+  	var gameId = players.get(socket.id);
+  	game = liveGames.get(gameId);
+  	if (game){
+  		console.log(game.players.get(socket.id), game.roundStartingPlayer, game.roundPlayersPlayed);
+	  	if(game.players.get(socket.id) == game.order[game.roundStartingPlayer + game.roundPlayersPlayed]){
+	  		if(game.roundPlayersPlayed == 0) {
+	  			game.roundNumCards = 0;//to fix for actually adjusting!
+	  			game.cardsPlayed += game.roundNumCards;
+	  		}
+	  		game.roundPlayersPlayed ++;
+	  	}
+	  //logic for legal play
+  	}
+  });
  });
+// async function runGame(gameId)
 
-async function sendCards(game){
+async function newGame(gameId){
+	game = liveGames.get(gameId);
+	var hand1;
+	var hand2;
+	var hand3;
+	var hand4;
+	var bottom;
+	[hand1, hand2, hand3, hand4, bottom] = initHands();
+	game.p1.set('hand', hand1);
+	game.p2.set('hand', hand2);
+	game.p3.set('hand', hand3);
+	game.p4.set('hand', hand4);
+	game.bottom = bottom;
+	console.log(game);
 	for(var i = 0; i < 25; i++){
-		io.to(game.p1.get('id')).emit('hand', game.p1.get('hand')[i]);
-		io.to(game.p2.get('id')).emit('hand', game.p2.get('hand')[i]);
-		await sleep(1);
+		io.to(game.p1.get('id')).emit('deal card', [game.p1.get('hand')[i], game.p1.get('level')]);
+		io.to(game.p2.get('id')).emit('deal card', [game.p2.get('hand')[i], game.p2.get('level')]);
+		// io.to(game.p3.get('id')).emit('hand', game.p3.get('hand')[i]);
+		// io.to(game.p4.get('id')).emit('hand', game.p4.get('hand')[i]);
+		await sleep(100);
 	}
-	io.to(game.id).emit('a', 'bug');
+	io.to(game.id).emit('cards dealt', game.id);
 	console.log('finished dealing to ', game.id);
+	//logic for declaring and countdown
+
+
+	if(!liveGames.get(game.id).trumpNum){
+		liveGames.get(game.id).trumpNum = 2;
+		liveGames.get(game.id).trumpSuit = 2;
+	}
+	
+	io.to(game.p1.get('id')).emit('finalize hand', [game.p1.get('hand'), game.trumpSuit, game.trumpNum]);
+	io.to(game.p2.get('id')).emit('finalize hand', [game.p2.get('hand'), game.trumpSuit, game.trumpNum]);
+	// io.to(game.p3.get('id')).emit('hand', game.p3.get('hand')[i]);
+	// io.to(game.p4.get('id')).emit('hand', game.p4.get('hand')[i]);
+	
+	//logic for getting the bottom cards
+	// console.log()
+	while(game.cardsPlayed <25) {
+		var playersPlayed = 0;
+		while(playersPlayed < 2) {
+			var t = 0;
+			io.to(game.id).emit('turn', game.roundStartingPlayer + playersPlayed);
+			while(t < 30) {
+				await sleep(1000);
+				io.to(game.id).emit('time', t);
+				t++;
+				console.log(game.roundPlayersPlayed, playersPlayed);
+				if(game.roundPlayersPlayed > playersPlayed) {
+					break;
+				}
+			}
+			if(t == 30) {
+				//logic for random card played
+				console.log('ran out of time!');
+			}
+			playersPlayed++;
+		}
+		game.roundPlayersPlayed = 0;
+	}
 }
 
 function shuffle (array) {
 // from https://gomakethings.com/how-to-shuffle-an-array-with-vanilla-js/
 	var currentIndex = array.length;
 	var temporaryValue, randomIndex;
-
 	// While there remain elements to shuffle...
 	while (0 !== currentIndex) {
 		// Pick a remaining element...
 		randomIndex = Math.floor(Math.random() * currentIndex);
 		currentIndex -= 1;
-
 		// And swap it with the current element.
 		temporaryValue = array[currentIndex];
 		array[currentIndex] = array[randomIndex];
 		array[randomIndex] = temporaryValue;
 	}
 	return array;
-
 };
 
-function createCard(suit, value) {
+function createCard(suit, value, deck) {
 	var obj = {};
 	obj.suit = suit;
 	obj.value = value;
+	obj.deck = deck
+	obj.power = value;
+	if(value == 'bigJoker'){
+		obj.power = 18;
+	}
+	if(value == 'smallJoker') {
+		obj.power = 17;
+	}
+	if(value == 5) {
+		obj.points = 5;
+	}
+	else if(value == 10 || value == 13) {
+		obj.points = 10;
+	}
+	else {
+		obj.points = 0;
+	}
 	return obj;
 }
 
 function sleep(ms){
-    return new Promise(resolve=>{
-        setTimeout(resolve,ms)
-    })
+  return new Promise(resolve=>{
+    setTimeout(resolve, ms);
+  });
 }
 
 function initHands() {
 	var cards = [];
-	cards.push(createCard('trump', 'bigJoker'));
-	cards.push(createCard('trump', 'bigJoker'));
-	cards.push(createCard('trump', 'smallJoker'));
-	cards.push(createCard('trump', 'smallJoker'));
-	for(var suit = 1; suit < 5; suit++) {
+	for(var deck = 1; deck < 3; deck++) {
 		for(var value = 2; value < 15; value++){
-			cards.push(createCard(suit, value));
-			cards.push(createCard(suit, value));
+			for(var suit = 1; suit < 5; suit++){
+				cards.push(createCard(suit, value, deck));
+			}
 		}
+		cards.push(createCard('T', 'bigJoker', deck));
+		cards.push(createCard('T', 'smallJoker', deck));
 	}
 	
 	cards = shuffle(cards);
-	// console.log(cards);
+	console.log(cards);
 	// console.log(cards.length);
 	var hand1 = cards.slice(0,25);
 	var hand2 = cards.slice(25,50);
